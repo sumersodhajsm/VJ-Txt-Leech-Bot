4448d97702a718896e589c225b8dbf434878203a
sumersodhajsm <sumersodha4321@gmail.com>
2025-02-21 18:32:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -166,8 +166,8 @@ async def upload(bot: Client, m: Message):
 
             try:  
                 
-                cc = f'**[📽️] Vid_ID:** {str(count).zfill(3)}.** {𝗻𝗮𝗺𝗲𝟭}{MR}.mkv\n**𝔹ᴀᴛᴄʜ** » **{raw_text0}**'
-                cc1 = f'**[📁] Pdf_ID:** {str(count).zfill(3)}. {𝗻𝗮𝗺𝗲𝟭}{MR}.pdf \n**𝔹ᴀᴛᴄʜ** » **{raw_text0}**DOWNLOAD BYE {𝗻𝗮𝗺𝗲𝟭}**'
+                cc =────────── f'**[📽️]────────── Vid_ID:** {str(count).zfill(3)}.** {𝗻𝗮𝗺𝗲𝟭}{MR}.mkv\n\n**𝔹ᴀᴛᴄʜ** » **{raw_text0}\n\n**DOWNLOAD BYE {𝗻𝗮𝗺𝗲𝟭}**'
+                cc1 = ──────────f'**[📁]────────── Pdf_ID:** {str(count).zfill(3)}. {𝗻𝗮𝗺𝗲𝟭}{MR}.pdf \n\n**𝔹ᴀᴛᴄʜ** » **{raw_text0}\n\n**DOWNLOAD BYE {𝗻𝗮𝗺𝗲𝟭}**'
                 if "drive" in url:
                     try:
                         ka = await helper.download(url, name)
